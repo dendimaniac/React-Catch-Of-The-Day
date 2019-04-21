64cf6fa09caafad94775ae0a750ab696bcad10bd
Quan Dao <daq30899@gmail.com>
2019-04-21 18:52:52 +0300

    Updates

---
+++ precache-manifest.2b66d0de9e641ec01580f1ed5f77bd23.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "e9535f20ca175cc3fdfe2816f7c47168",
+    "url": "/React-Catch-Of-The-Day/static/media/haymaker-webfont.e9535f20.ttf"
+  },
+  {
+    "revision": "a18c8ee9489f75239d57",
+    "url": "/React-Catch-Of-The-Day/static/css/main.6ede7ad2.chunk.css"
+  },
+  {
+    "revision": "32765e1f8e997b861bfa",
+    "url": "/React-Catch-Of-The-Day/static/js/runtime~main.c7f6f010.js"
+  },
+  {
+    "revision": "8617a5eb17f1d78b39c2",
+    "url": "/React-Catch-Of-The-Day/static/js/2.a28c9455.chunk.js"
+  },
+  {
+    "revision": "751e56274d3ea1c651ea824f902bb987",
+    "url": "/React-Catch-Of-The-Day/static/media/haymaker-webfont.751e5627.woff"
+  },
+  {
+    "revision": "4c3fb584d3f63821357c8a9cff97f360",
+    "url": "/React-Catch-Of-The-Day/static/media/haymaker-webfont.4c3fb584.eot"
+  },
+  {
+    "revision": "d2f8799a13e6db2022d9a7de22776705",
+    "url": "/React-Catch-Of-The-Day/static/media/anchor.d2f8799a.svg"
+  },
+  {
+    "revision": "a18c8ee9489f75239d57",
+    "url": "/React-Catch-Of-The-Day/static/js/main.db2385d7.chunk.js"
+  },
+  {
+    "revision": "cf798e30eda73d84b25c4ae233fe5e21",
+    "url": "/React-Catch-Of-The-Day/static/media/haymaker-webfont.cf798e30.svg"
+  },
+  {
+    "revision": "73a576a8254aeafebcdc66fe9fd41ac8",
+    "url": "/React-Catch-Of-The-Day/static/media/blanch_caps_inline-webfont.73a576a8.eot"
+  },
+  {
+    "revision": "1461f8e9d2084c70c00830348efe1766",
+    "url": "/React-Catch-Of-The-Day/static/media/blanch_caps_inline-webfont.1461f8e9.woff"
+  },
+  {
+    "revision": "8345e1b3362c1d640f0f11b5aae1d5bc",
+    "url": "/React-Catch-Of-The-Day/static/media/blanch_caps_inline-webfont.8345e1b3.ttf"
+  },
+  {
+    "revision": "7bbffda6bcacac8360f0ee00bb03dc04",
+    "url": "/React-Catch-Of-The-Day/static/media/blanch_caps_inline-webfont.7bbffda6.svg"
+  },
+  {
+    "revision": "98cab6b42d81d530f8f949019f3ccdab",
+    "url": "/React-Catch-Of-The-Day/index.html"
+  }
+];
